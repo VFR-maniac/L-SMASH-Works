VFR-maniac/L-SMASH-Works

commit b79372b9d6b778811c2315c7539d0332c114b8db

lwlibav: Remove packet input buffer.

AviSynth/lwlibav_source.cpp
@@ -112,11 +112,7 @@ LWLibavVideoSource::~LWLibavVideoSource()
 
 void LWLibavVideoSource::prepare_video_decoding( IScriptEnvironment *env )
 {
-    vdh.eh.message_priv    = env;
-    vdh.input_buffer_size += FF_INPUT_BUFFER_PADDING_SIZE;
-    vdh.input_buffer       = (uint8_t *)av_mallocz( vdh.input_buffer_size );
-    if( !vdh.input_buffer )
-        env->ThrowError( "LWLibavVideoSource: failed to allocate memory to the input buffer for video." );
+    vdh.eh.message_priv = env;
     /* Import AVIndexEntrys. */
     if( vdh.index_entries )
     {
@@ -166,11 +162,12 @@ void LWLibavVideoSource::prepare_video_decoding( IScriptEnvironment *env )
     for( uint32_t i = 1; i <= vi.num_frames + get_decoder_delay( vdh.ctx ); i++ )
     {
         AVPacket pkt = { 0 };
-        get_av_frame( vdh.format, vdh.stream_index, &vdh.input_buffer, &vdh.input_buffer_size, &pkt );
-        AVFrame *picture = vdh.frame_buffer;
-        avcodec_get_frame_defaults( picture );
+        lw_get_av_frame( vdh.format, vdh.stream_index, &pkt );
+        avcodec_get_frame_defaults( vdh.frame_buffer );
         int got_picture;
-        if( avcodec_decode_video2( vdh.ctx, picture, &got_picture, &pkt ) >= 0 && got_picture )
+        int consumed_bytes = avcodec_decode_video2( vdh.ctx, vdh.frame_buffer, &got_picture, &pkt );
+        av_free_packet( &pkt );
+        if( consumed_bytes >= 0 && got_picture )
         {
             voh.first_valid_frame_number = i - MIN( get_decoder_delay( vdh.ctx ), vdh.delay_count );
             if( voh.first_valid_frame_number > 1 || vi.num_frames == 1 )
@@ -178,7 +175,7 @@ void LWLibavVideoSource::prepare_video_decoding( IScriptEnvironment *env )
                 PVideoFrame temp = env->NewVideoFrame( vi );
                 if( !temp )
                     env->ThrowError( "LWLibavVideoSource: failed to allocate memory for the first valid video frame data." );
-                if( make_frame( &voh, picture, temp, env ) < 0 )
+                if( make_frame( &voh, vdh.frame_buffer, temp, env ) < 0 )
                     continue;
                 voh.first_valid_frame = new PVideoFrame( temp );
                 if( !voh.first_valid_frame )
@@ -253,12 +250,8 @@ LWLibavAudioSource::~LWLibavAudioSource()
 
 void LWLibavAudioSource::prepare_audio_decoding( IScriptEnvironment *env )
 {
-    adh.eh.message_priv    = env;
-    adh.input_buffer_size += FF_INPUT_BUFFER_PADDING_SIZE;
-    adh.input_buffer       = (uint8_t *)av_mallocz( adh.input_buffer_size );
-    if( !adh.input_buffer )
-        env->ThrowError( "LWLibavAudioSource: failed to allocate memory to the input buffer for audio." );
-    /* */
+    adh.eh.message_priv = env;
+    /* Count the number of PCM audio samples. */
     vi.num_audio_samples = lwlibav_count_overall_pcm_samples( &adh, aoh.output_sample_rate );
     if( vi.num_audio_samples == 0 )
         env->ThrowError( "LWLibavAudioSource: no valid audio frame." );

AviUtl/libav_input.c
@@ -159,13 +159,6 @@ static int prepare_video_decoding( lsmash_handler_t *h, video_option_t *opt )
         return 0;
     vdhp->seek_mode              = opt->seek_mode;
     vdhp->forward_seek_threshold = opt->forward_seek_threshold;
-    vdhp->input_buffer_size     += FF_INPUT_BUFFER_PADDING_SIZE;
-    vdhp->input_buffer           = av_mallocz( vdhp->input_buffer_size );
-    if( !vdhp->input_buffer )
-    {
-        DEBUG_VIDEO_MESSAGE_BOX_DESKTOP( MB_ICONERROR | MB_OK, "Failed to allocate memory to the input buffer for video." );
-        return -1;
-    }
     h->video_sample_count = vdhp->frame_count;
     /* Import AVIndexEntrys. */
     if( vdhp->index_entries )
@@ -263,10 +256,12 @@ static int prepare_video_decoding( lsmash_handler_t *h, video_option_t *opt )
     for( uint32_t i = 1; i <= h->video_sample_count + get_decoder_delay( vdhp->ctx ); i++ )
     {
         AVPacket pkt = { 0 };
-        get_av_frame( vdhp->format, vdhp->stream_index, &vdhp->input_buffer, &vdhp->input_buffer_size, &pkt );
+        lw_get_av_frame( vdhp->format, vdhp->stream_index, &pkt );
         avcodec_get_frame_defaults( vdhp->frame_buffer );
         int got_picture;
-        if( avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt ) >= 0 && got_picture )
+        int consumed_bytes = avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt );
+        av_free_packet( &pkt );
+        if( consumed_bytes >= 0 && got_picture )
         {
             vohp->first_valid_frame_number = i - MIN( get_decoder_delay( vdhp->ctx ), vdhp->delay_count );
             if( vohp->first_valid_frame_number > 1 || h->video_sample_count == 1 )
@@ -296,13 +291,6 @@ static int prepare_audio_decoding( lsmash_handler_t *h )
     lwlibav_audio_decode_handler_t *adhp = &hp->adh;
     if( !adhp->ctx )
         return 0;
-    adhp->input_buffer_size += FF_INPUT_BUFFER_PADDING_SIZE;
-    adhp->input_buffer       = av_mallocz( adhp->input_buffer_size );
-    if( !adhp->input_buffer )
-    {
-        DEBUG_AUDIO_MESSAGE_BOX_DESKTOP( MB_ICONERROR | MB_OK, "Failed to allocate memory to the input buffer for audio." );
-        return -1;
-    }
     lwlibav_audio_output_handler_t *aohp = &hp->aoh;
     h->audio_pcm_sample_count = lwlibav_count_overall_pcm_samples( adhp, aohp->output_sample_rate );
     if( h->audio_pcm_sample_count == 0 )

VapourSynth/lwlibav_source.c
@@ -98,14 +98,6 @@ static int prepare_video_decoding( lwlibav_handler_t *hp, VSCore *core, const VS
     lwlibav_video_output_handler_t *vohp  = &hp->voh;
     VSVideoInfo                    *vi    = &hp->vi;
     vs_basic_handler_t             *vsbhp = (vs_basic_handler_t *)vdhp->eh.message_priv;
-    /* Allocate video frame buffer. */
-    vdhp->input_buffer_size += FF_INPUT_BUFFER_PADDING_SIZE;
-    vdhp->input_buffer       = (uint8_t *)av_mallocz( vdhp->input_buffer_size );
-    if( !vdhp->input_buffer )
-    {
-        set_error( vsbhp, "lsmas: failed to allocate video frame buffer." );
-        return -1;
-    }
     /* Import AVIndexEntrys. */
     if( vdhp->index_entries )
     {
@@ -177,10 +169,12 @@ static int prepare_video_decoding( lwlibav_handler_t *hp, VSCore *core, const VS
     for( uint32_t i = 1; i <= vi->numFrames + get_decoder_delay( vdhp->ctx ); i++ )
     {
         AVPacket pkt = { 0 };
-        get_av_frame( vdhp->format, vdhp->stream_index, &vdhp->input_buffer, &vdhp->input_buffer_size, &pkt );
+        lw_get_av_frame( vdhp->format, vdhp->stream_index, &pkt );
         avcodec_get_frame_defaults( vdhp->frame_buffer );
         int got_picture;
-        if( avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt ) >= 0 && got_picture )
+        int consumed_bytes = avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt );
+        av_free_packet( &pkt );
+        if( consumed_bytes >= 0 && got_picture )
         {
             vohp->first_valid_frame_number = i - MIN( get_decoder_delay( vdhp->ctx ), vdhp->delay_count );
             if( vohp->first_valid_frame_number > 1 || vi->numFrames == 1 )

common/audio_output.c
@@ -120,26 +120,28 @@ uint64_t output_pcm_samples_from_packet
 {
     uint64_t output_length = 0;
     int      output_audio  = 0;
+    AVPacket alter_pkt     = *pkt;
     do
     {
         avcodec_get_frame_defaults( frame_buffer );
         int decode_complete;
-        int wasted_data_length = avcodec_decode_audio4( ctx, frame_buffer, &decode_complete, pkt );
-        if( wasted_data_length < 0 )
+        int consumed_data_length = avcodec_decode_audio4( ctx, frame_buffer, &decode_complete, &alter_pkt );
+        if( consumed_data_length < 0 )
         {
-            pkt->size = 0;  /* Force to request the next sample. */
+            /* Force to request the next sample. */
+            alter_pkt.size = 0;
             break;
         }
-        if( pkt->data )
+        if( alter_pkt.data )
         {
-            pkt->size -= wasted_data_length;
-            pkt->data += wasted_data_length;
+            alter_pkt.size -= consumed_data_length;
+            alter_pkt.data += consumed_data_length;
         }
         else if( !decode_complete )
         {
             /* No more PCM audio samples in this stream. */
             *output_flags |= AUDIO_OUTPUT_ENOUGH;
-            return output_length;
+            break;
         }
         output_audio |= decode_complete ? 1 : 0;
         if( decode_complete
@@ -194,9 +196,17 @@ uint64_t output_pcm_samples_from_packet
             else
                 aohp->output_sample_offset -= decoded_length;
         }
-    } while( pkt->size > 0 );
+    } while( alter_pkt.size > 0 );
     if( !output_audio && pkt->data )
         /* Count audio frame delay only if feeding non-NULL packet. */
         *output_flags |= AUDIO_DECODER_DELAY;
+    if( alter_pkt.size > 0 )
+    {
+        uint8_t *temp = pkt->data;
+        *pkt = alter_pkt;
+        pkt->data = temp;
+    }
+    else
+        av_free_packet( pkt );
     return output_length;
 }

common/lwindex.c
@@ -566,7 +566,6 @@ static void create_index
                 vdhp->ctx               = pkt_ctx;
                 vdhp->codec_id          = pkt_ctx->codec_id;
                 vdhp->stream_index      = pkt.stream_index;
-                vdhp->input_buffer_size = 0;
                 video_resolution        = pkt_ctx->width * pkt_ctx->height;
                 video_sample_count      = 0;
                 last_keyframe_pts       = AV_NOPTS_VALUE;
@@ -604,7 +603,6 @@ static void create_index
                     }
                     video_info = temp;
                 }
-                vdhp->input_buffer_size = MAX( vdhp->input_buffer_size, (uint32_t)pkt.size );
             }
             /* Write a video packet info to the index file. */
             print_index( index, "Index=%d,Type=%d,Codec=%d,TimeBase=%d/%d,POS=%"PRId64",PTS=%"PRId64",DTS=%"PRId64"\n"
@@ -718,7 +716,6 @@ static void create_index
                     aohp->output_sample_format   = select_better_sample_format( aohp->output_sample_format, pkt_ctx->sample_fmt );
                     aohp->output_sample_rate     = MAX( aohp->output_sample_rate, audio_sample_rate );
                     aohp->output_bits_per_sample = MAX( aohp->output_bits_per_sample, bits_per_sample );
-                    adhp->input_buffer_size      = MAX( adhp->input_buffer_size, (uint32_t)pkt.size );
                 }
             }
             /* Write an audio packet info to the index file. */

common/lwlibav_audio.c
@@ -177,7 +177,8 @@ static void seek_audio
     /* Seek to the target audio frame and get it. */
     for( uint32_t i = rap_number; i <= frame_number; )
     {
-        if( get_av_frame( adhp->format, adhp->stream_index, &adhp->input_buffer, &adhp->input_buffer_size, pkt ) )
+        av_free_packet( pkt );
+        if( lw_get_av_frame( adhp->format, adhp->stream_index, pkt ) )
             break;
         if( i == rap_number
          && (((adhp->seek_base & SEEK_FILE_OFFSET_BASED) && (pkt->pos == -1 || adhp->frame_list[i].file_offset > pkt->pos))
@@ -230,6 +231,7 @@ uint64_t lwlibav_get_pcm_audio_samples
             return 0;
         }
         flush_buffers( adhp->ctx, &adhp->eh );
+        av_free_packet( pkt );
         if( adhp->eh.error )
             return 0;
         adhp->delay_count            = 0;
@@ -269,7 +271,7 @@ uint64_t lwlibav_get_pcm_audio_samples
         }
         else if( pkt->size <= 0 )
             /* Getting an audio packet must be after flushing all remaining samples in resampler's FIFO buffer. */
-            get_av_frame( adhp->format, adhp->stream_index, &adhp->input_buffer, &adhp->input_buffer_size, pkt );
+            lw_get_av_frame( adhp->format, adhp->stream_index, pkt );
         /* Decode and output from an audio packet. */
         output_flags   = AUDIO_OUTPUT_NO_FLAGS;
         output_length += output_pcm_samples_from_packet( aohp, adhp->ctx, pkt, adhp->frame_buffer, (uint8_t **)&buf, &output_flags );
@@ -296,8 +298,7 @@ audio_out:
 
 void lwlibav_cleanup_audio_decode_handler( lwlibav_audio_decode_handler_t *adhp )
 {
-    if( adhp->input_buffer )
-        av_freep( &adhp->input_buffer );
+    av_free_packet( &adhp->packet );
     if( adhp->index_entries )
         av_freep( &adhp->index_entries );
     if( adhp->frame_buffer )

common/lwlibav_audio.h
@@ -47,8 +47,6 @@ typedef struct
     enum AVCodecID      codec_id;
     uint32_t            frame_count;
     uint32_t            delay_count;
-    uint32_t            input_buffer_size;
-    uint8_t            *input_buffer;
     AVFrame            *frame_buffer;
     AVPacket            packet;
     audio_frame_info_t *frame_list;

common/lwlibav_dec.c
@@ -34,43 +34,24 @@ extern "C"
 
 #include "lwlibav_dec.h"
 
-int get_av_frame
+int lw_get_av_frame
 (
     AVFormatContext *format_ctx,
     int              stream_index,
-    uint8_t        **buffer,
-    uint32_t        *buffer_size,
     AVPacket        *pkt
 )
 {
-    AVPacket temp = { 0 };
-    av_init_packet( &temp );
-    while( read_av_frame( format_ctx, &temp ) >= 0 )
+    av_init_packet( pkt );
+    while( read_av_frame( format_ctx, pkt ) >= 0 )
     {
-        if( temp.stream_index != stream_index )
+        if( pkt->stream_index != stream_index )
         {
-            av_free_packet( &temp );
+            av_free_packet( pkt );
             continue;
         }
-        /* Don't trust the first survey of the maximum packet size. It seems various by seeking. */
-        if( ((unsigned int)temp.size + FF_INPUT_BUFFER_PADDING_SIZE) > *buffer_size )
-        {
-            uint8_t *new_buffer = (uint8_t *)av_realloc( *buffer, temp.size + FF_INPUT_BUFFER_PADDING_SIZE );
-            if( !new_buffer )
-            {
-                av_free_packet( &temp );
-                continue;
-            }
-            *buffer      = new_buffer;
-            *buffer_size = temp.size + FF_INPUT_BUFFER_PADDING_SIZE;
-        }
-        *pkt = temp;
-        pkt->data = *buffer;
-        memcpy( pkt->data, temp.data, temp.size );
-        av_free_packet( &temp );
         return 0;
     }
-    *pkt = temp;
+    /* Return a null packet. */
     pkt->data = NULL;
     pkt->size = 0;
     return 1;

commit d30de7afcd0893b84717753f557fc9bd44f05f00

lwlibav: Fix a regression in b79372b9d6 .
The number of decoder delay was not counted correctly when finding the first valid video frame.

AviSynth/lwlibav_source.cpp
@@ -166,6 +166,7 @@ void LWLibavVideoSource::prepare_video_decoding( IScriptEnvironment *env )
         avcodec_get_frame_defaults( vdh.frame_buffer );
         int got_picture;
         int consumed_bytes = avcodec_decode_video2( vdh.ctx, vdh.frame_buffer, &got_picture, &pkt );
+        int is_real_packet = pkt.data ? 1 : 0;
         av_free_packet( &pkt );
         if( consumed_bytes >= 0 && got_picture )
         {
@@ -183,7 +184,7 @@ void LWLibavVideoSource::prepare_video_decoding( IScriptEnvironment *env )
             }
             break;
         }
-        else if( pkt.data )
+        else if( is_real_packet )
             ++ vdh.delay_count;
     }
     vdh.last_frame_number = vi.num_frames + 1;  /* Force seeking at the first reading. */

AviUtl/libav_input.c
@@ -260,6 +260,7 @@ static int prepare_video_decoding( lsmash_handler_t *h, video_option_t *opt )
         avcodec_get_frame_defaults( vdhp->frame_buffer );
         int got_picture;
         int consumed_bytes = avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt );
+        int is_real_packet = pkt.data ? 1 : 0;
         av_free_packet( &pkt );
         if( consumed_bytes >= 0 && got_picture )
         {
@@ -278,7 +279,7 @@ static int prepare_video_decoding( lsmash_handler_t *h, video_option_t *opt )
             }
             break;
         }
-        else if( pkt.data )
+        else if( is_real_packet )
             ++ vdhp->delay_count;
     }
     vdhp->last_frame_number = h->video_sample_count + 1;    /* Force seeking at the first reading. */

VapourSynth/lwlibav_source.c
@@ -173,6 +173,7 @@ static int prepare_video_decoding( lwlibav_handler_t *hp, VSCore *core, const VS
         avcodec_get_frame_defaults( vdhp->frame_buffer );
         int got_picture;
         int consumed_bytes = avcodec_decode_video2( vdhp->ctx, vdhp->frame_buffer, &got_picture, &pkt );
+        int is_real_packet = pkt.data ? 1 : 0;
         av_free_packet( &pkt );
         if( consumed_bytes >= 0 && got_picture )
         {
@@ -194,7 +195,7 @@ static int prepare_video_decoding( lwlibav_handler_t *hp, VSCore *core, const VS
             }
             break;
         }
-        else if( pkt.data )
+        else if( is_real_packet )
             ++ vdhp->delay_count;
     }
     vdhp->last_frame_number = vi->numFrames + 1;    /* Force seeking at the first reading. */