VFR-maniac/L-SMASH-Works

commit 66345ef6d075a12c62b1f8a3eefd12ea1c3a6a95

common: Integrate the codes of cleaning up video output handler.

AviSynth/libavsmash_source.cpp
@@ -76,17 +76,12 @@ LSMASHVideoSource::LSMASHVideoSource
 
 LSMASHVideoSource::~LSMASHVideoSource()
 {
-    if( voh.first_valid_frame )
-        delete voh.first_valid_frame;
-    if( voh.scaler.sws_ctx )
-        sws_freeContext( voh.scaler.sws_ctx );
-    if( voh.free_private_handler && voh.private_handler )
-        voh.free_private_handler( &voh.private_handler );
     if( vdh.order_converter )
         delete [] vdh.order_converter;
     if( vdh.frame_buffer )
         avcodec_free_frame( &vdh.frame_buffer );
     cleanup_configuration( &vdh.config );
+    libavsmash_cleanup_video_output_handler( &voh );
     if( format_ctx )
         avformat_close_input( &format_ctx );
     lsmash_destroy_root( vdh.root );

AviSynth/lwlibav_source.cpp
@@ -98,13 +98,8 @@ LWLibavVideoSource::LWLibavVideoSource
 
 LWLibavVideoSource::~LWLibavVideoSource()
 {
-    if( voh.first_valid_frame )
-        delete voh.first_valid_frame;
-    if( voh.scaler.sws_ctx )
-        sws_freeContext( voh.scaler.sws_ctx );
-    if( voh.free_private_handler && voh.private_handler )
-        voh.free_private_handler( &voh.private_handler );
     lwlibav_cleanup_video_decode_handler( &vdh );
+    lwlibav_cleanup_video_output_handler( &voh );
     if( lwh.file_path )
         free( lwh.file_path );
 }

AviSynth/video_output.cpp
@@ -353,6 +353,16 @@ fail:
     return AVERROR( ENOMEM );
 }
 
+static void as_free_first_valid_frame
+(
+    void *first_valid_frame,
+    void *private_handler
+)
+{
+    if( first_valid_frame )
+        delete first_valid_frame;
+}
+
 func_get_buffer_t *as_setup_video_rendering
 (
     lw_video_output_handler_t *vohp,
@@ -390,5 +400,6 @@ func_get_buffer_t *as_setup_video_rendering
     }
     vohp->output_width  = vi->width;
     vohp->output_height = vi->height;
+    vohp->free_first_valid_frame = as_free_first_valid_frame;
     return ctx->get_buffer2;
 }

AviUtl/libav_input.c
@@ -281,13 +281,8 @@ static void video_cleanup( lsmash_handler_t *h )
     libav_handler_t *hp = (libav_handler_t *)h->video_private;
     if( !hp )
         return;
-    if( hp->voh.first_valid_frame )
-        free( hp->voh.first_valid_frame );
-    if( hp->voh.scaler.sws_ctx )
-        sws_freeContext( hp->voh.scaler.sws_ctx );
-    if( hp->voh.free_private_handler && hp->voh.private_handler )
-        hp->voh.free_private_handler( hp->voh.private_handler );
     lwlibav_cleanup_video_decode_handler( &hp->vdh );
+    lwlibav_cleanup_video_output_handler( &hp->voh );
 }
 
 static void audio_cleanup( lsmash_handler_t *h )

AviUtl/libavsmash_input.c
@@ -578,15 +578,12 @@ static void video_cleanup( lsmash_handler_t *h )
         return;
     if( hp->vih.keyframe_list )
         free( hp->vih.keyframe_list );
-    if( hp->voh.scaler.sws_ctx )
-        sws_freeContext( hp->voh.scaler.sws_ctx );
-    if( hp->voh.free_private_handler && hp->voh.private_handler )
-        hp->voh.free_private_handler( hp->voh.private_handler );
     if( hp->vdh.order_converter )
         free( hp->vdh.order_converter );
     if( hp->vdh.frame_buffer )
         avcodec_free_frame( &hp->vdh.frame_buffer );
     cleanup_configuration( &hp->vdh.config );
+    libavsmash_cleanup_video_output_handler( &hp->voh );
 }
 
 static void audio_cleanup( lsmash_handler_t *h )

AviUtl/video_output.c
@@ -108,7 +108,10 @@ static output_colorspace_index determine_colorspace_conversion
     }
 }
 
-static void free_au_video_output_handler( void *private_handler )
+static void au_free_video_output_handler
+(
+    void *private_handler
+)
 {
     au_video_output_handler_t *au_vohp = (au_video_output_handler_t *)private_handler;
     if( !au_vohp )
@@ -116,6 +119,16 @@ static void free_au_video_output_handler( void *private_handler )
     if( au_vohp->back_ground )
         free( au_vohp->back_ground );
     free( au_vohp );
+}
+
+static void au_free_first_valid_frame
+(
+    void *first_valid_frame,
+    void *private_handler
+)
+{
+    if( first_valid_frame )
+        free( first_valid_frame );
 }
 
 func_get_buffer_t *au_setup_video_rendering
@@ -135,8 +148,9 @@ func_get_buffer_t *au_setup_video_rendering
         DEBUG_VIDEO_MESSAGE_BOX_DESKTOP( MB_ICONERROR | MB_OK, "Failed to allocate the AviUtl video output handler." );
         return NULL;
     }
-    vohp->private_handler      = au_vohp;
-    vohp->free_private_handler = free_au_video_output_handler;
+    vohp->private_handler        = au_vohp;
+    vohp->free_private_handler   = au_free_video_output_handler;
+    vohp->free_first_valid_frame = au_free_first_valid_frame;
     enum AVPixelFormat output_pixel_format;
     output_colorspace_index index = determine_colorspace_conversion( ctx->pix_fmt, &output_pixel_format );
     if( initialize_scaler_handler( &vohp->scaler, ctx, 1, 1 << opt->scaler, output_pixel_format ) < 0 )

VapourSynth/libavsmash_source.c
@@ -242,17 +242,12 @@ static void VS_CC vs_filter_free( void *instance_data, VSCore *core, const VSAPI
     lsmas_handler_t *hp = (lsmas_handler_t *)instance_data;
     if( !hp )
         return;
-    if( hp->voh.first_valid_frame )
-        vsapi->freeFrame( hp->voh.first_valid_frame );
     if( hp->vdh.order_converter )
         free( hp->vdh.order_converter );
     if( hp->vdh.frame_buffer )
         avcodec_free_frame( &hp->vdh.frame_buffer );
-    if( hp->voh.scaler.sws_ctx )
-        sws_freeContext( hp->voh.scaler.sws_ctx );
-    if( hp->voh.free_private_handler && hp->voh.private_handler )
-        hp->voh.free_private_handler( hp->voh.private_handler );
     cleanup_configuration( &hp->vdh.config );
+    libavsmash_cleanup_video_output_handler( &hp->voh );
     if( hp->format_ctx )
         avformat_close_input( &hp->format_ctx );
     lsmash_destroy_root( hp->vdh.root );

VapourSynth/lwlibav_source.c
@@ -237,12 +237,7 @@ static void VS_CC vs_filter_free( void *instance_data, VSCore *core, const VSAPI
     if( !hp )
         return;
     lwlibav_cleanup_video_decode_handler( &hp->vdh );
-    if( hp->voh.first_valid_frame )
-        vsapi->freeFrame( hp->voh.first_valid_frame );
-    if( hp->voh.scaler.sws_ctx )
-        sws_freeContext( hp->voh.scaler.sws_ctx );
-    if( hp->voh.free_private_handler && hp->voh.private_handler )
-        hp->voh.free_private_handler( hp->voh.private_handler );
+    lwlibav_cleanup_video_output_handler( &hp->voh );
     if( hp->lwh.file_path )
         free( hp->lwh.file_path );
     free( hp );

commit 99d336735d85f210f8f4dfa87dfaaf54f559b753

lsmashsource: Fix freeing the private output handler.
This is a regression in 66345ef6d0.

AviSynth/libavsmash_source.cpp
@@ -67,7 +67,7 @@ LSMASHVideoSource::LSMASHVideoSource
     as_vohp->vi  = &vi;
     as_vohp->env = env;
     voh.private_handler      = as_vohp;
-    voh.free_private_handler = lw_freep;
+    voh.free_private_handler = free;
     get_video_track( source, track_number, threads, env );
     lsmash_discard_boxes( vdh.root );
     prepare_video_decoding( direct_rendering, env );

AviSynth/lwlibav_source.cpp
@@ -63,7 +63,7 @@ LWLibavVideoSource::LWLibavVideoSource
     as_vohp->vi  = &vi;
     as_vohp->env = env;
     voh.private_handler      = as_vohp;
-    voh.free_private_handler = lw_freep;
+    voh.free_private_handler = free;
     /* Set up error handler. */
     lw_log_handler_t lh;
     lh.level    = LW_LOG_FATAL; /* Ignore other than fatal error. */