0ec3ffd18a19218c121e743584c1741b0ecedb86
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2013-04-12 03:04:00 +0900

    lsmashmuxer: Forgot to change the information of filter plugin.

--- AviUtl/lsmashmuxer.c
+++ AviUtl/lsmashmuxer.c
@@ -96,7 +96,7 @@ FILTER_DLL filter =
     NULL,                                               /* Pointer to group of set points of checkbox */
     NULL,                                               /* Pointer to extended data region (Valid only if FILTER_FLAG_EX_DATA is enabled.) */
     0,                                                  /* Size of extended data (Valid only if FILTER_FLAG_EX_DATA is enabled.) */
-    "Libav-SMASH Muxer r" LSMASHWORKS_REV "\0",         /* Information of filter plugin */
+    "L-SMASH Works Muxer r" LSMASHWORKS_REV "\0",       /* Information of filter plugin */
 };
 
 EXTERN_C FILTER_DLL __declspec(dllexport) * __stdcall GetFilterTable( void )
