VFR-maniac/L-SMASH-Works

commit 85e2b16a8a309b4f2186dbdf0d037f124a5d6883

lsmashmuxer: Rename lsmashmuxer.auf into lwmuxer.auf.

AviUtl/lwmuxer.c
@@ -1371,7 +1371,7 @@ static int finish_movie( output_movie_t *output, int optimize_pd )
     if( !optimize_pd )
         return lsmash_finish_movie( output->root, NULL );
     progress_dlg_t progress_dlg;
-    init_progress_dlg( &progress_dlg, "lsmashmuxer.auf", IDD_PROGRESS_UNABORTABLE );
+    init_progress_dlg( &progress_dlg, "lwmuxer.auf", IDD_PROGRESS_UNABORTABLE );
     lsmash_adhoc_remux_t moov_to_front;
     moov_to_front.func        = moov_to_front_callback;
     moov_to_front.buffer_size = 4*1024*1024;    /* 4MiB */

AviUtl/GNUmakefile
@@ -9,7 +9,7 @@ vpath %.h $(SRCDIR)
 vpath %.rc $(SRCDIR)
 
 OBJ_INPUT = $(SRC_INPUT:%.c=%.o) lsmashinput.res
-OBJ_MUXER = $(SRC_MUXER:%.c=%.o) lsmashmuxer.res
+OBJ_MUXER = $(SRC_MUXER:%.c=%.o) lwmuxer.res
 OBJ_DUMPER = $(SRC_DUMPER:%.c=%.o)
 
 SRC_ALL = $(SRC_INPUT) $(SRC_MUXER) $(SRC_DUMPER)
@@ -26,14 +26,14 @@ all: input muxer dumper
 
 input: lsmashinput.aui
 
-muxer: lsmashmuxer.auf
+muxer: lwmuxer.auf
 
 dumper: lsmashdumper.auf
 
 lsmashinput.aui: $(OBJ_INPUT)
 	$(LD) $(LDFLAGS) -o $@ $^ $(LIBS)
 
-lsmashmuxer.auf: $(OBJ_MUXER)
+lwmuxer.auf: $(OBJ_MUXER)
 	$(LD) $(LDFLAGS) -o $@ $^ $(LIBS)
 
 lsmashdumper.auf: $(OBJ_DUMPER)

AviUtl/configure
@@ -89,7 +89,7 @@ SRC_INPUT="lsmashinput.c libavsmash_input.c libav_input.c avs_input.c dummy_inpu
            ../common/lwlibav_dec.c ../common/lwlibav_video.c ../common/lwlibav_audio.c       \
            ../common/lwindex.c ../common/resample.c ../common/audio_output.c                 \
            ../common/video_output.c ../common/utils.c"
-SRC_MUXER="lsmashmuxer.c progress_dlg.c ../common/utils.c"
+SRC_MUXER="lwmuxer.c progress_dlg.c ../common/utils.c"
 SRC_DUMPER="lsmashdumper.c"
 
 # -- options ----------------------------------------------------------------------------------
@@ -272,6 +272,6 @@ cat << EOF
 configure finished.
 type 'make'        : compile all plugins
 type 'make input'  : compile lsmashinput.aui
-type 'make muxer'  : compile lsmashmuxer.auf
+type 'make muxer'  : compile lwmuxer.auf
 type 'make dumper' : compile lsmashdumper.auf
 EOF

commit 7dd0a7f118d68cc276a543d2874ccea664ce2ab2

lwmuxer: Fix progress dialog.
This is a regression in 85e2b16a8a.

AviUtl/lwmuxer.c
@@ -1085,7 +1085,7 @@ static int do_mux( lsmash_handler_t *hp )
     uint32_t          num_output_samples[2]       = { 0, 0 };
     uint64_t          total_num_samples           = hp->number_of_samples[VIDEO_TRACK] + hp->number_of_samples[AUDIO_TRACK];
     progress_dlg_t    progress_dlg;
-    init_progress_dlg( &progress_dlg, "lsmashmuxer.auf", IDD_PROGRESS_ABORTABLE );
+    init_progress_dlg( &progress_dlg, "lwmuxer.auf", IDD_PROGRESS_ABORTABLE );
     while( 1 )
     {
         /* Try append a sample in an input track where we didn't reach the end of media timeline. */
@@ -1590,7 +1590,7 @@ BOOL func_WndProc( HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam, void *
             muxer_t muxer;
             muxer.editp = editp;
             muxer.fp    = fp;
-            DialogBoxParam( GetModuleHandle( "lsmashmuxer.auf" ), MAKEINTRESOURCE( IDD_MUXER_OPTIONS ), hwnd, dialog_proc, (LPARAM)&muxer );
+            DialogBoxParam( GetModuleHandle( "lwmuxer.auf" ), MAKEINTRESOURCE( IDD_MUXER_OPTIONS ), hwnd, dialog_proc, (LPARAM)&muxer );
             break;
         }
         case WM_FILTER_FILE_CLOSE :