VFR-maniac/L-SMASH-Works

commit b376363be520842f72fdffcc9cb2781f355936ca

libavsmash: Don't exceed the maximum presentation size specified for each sequence.

common/libavsmash.c
@@ -644,8 +644,8 @@ void update_configuration( lsmash_root_t *root, uint32_t track_ID, codec_configu
     if( codec->type == AVMEDIA_TYPE_VIDEO )
     {
         lsmash_video_summary_t *video = (lsmash_video_summary_t *)summary;
-        ctx->coded_width  = video->width;
-        ctx->coded_height = video->height;
+        ctx->width  = video->width;
+        ctx->height = video->height;
         if( codec->id == AV_CODEC_ID_RAWVIDEO )
             /* Here, expect appropriate pixel format will be picked in avcodec_open2(). */
             config->queue.bits_per_sample = video->depth;
@@ -700,6 +700,10 @@ void update_configuration( lsmash_root_t *root, uint32_t track_ID, codec_configu
     extended_summary_t *extended = &config->entries[ config->index - 1 ].extended;
     if( ctx->codec_type == AVMEDIA_TYPE_VIDEO )
     {
+        /* Set the maximum width and height in this sequence. */
+        extended->width  = ctx->width;
+        extended->height = ctx->height;
+        /* Actual decoding */
         uint32_t i = current_sample_number;
         do
         {
@@ -720,8 +724,6 @@ void update_configuration( lsmash_root_t *root, uint32_t track_ID, codec_configu
             int dummy;
             avcodec_decode_video2( ctx, picture, &dummy, &pkt );
         } while( ctx->width == 0 || ctx->height == 0 || ctx->pix_fmt == AV_PIX_FMT_NONE );
-        extended->width  = ctx->width;
-        extended->height = ctx->height;
     }
     else
     {

common/libavsmash.h
@@ -22,8 +22,8 @@
 
 typedef struct
 {
-    int                 width;
-    int                 height;
+    int                 width;      /* the maximum visual presentation width */
+    int                 height;     /* the maximum visual presentation height */
     uint64_t            channel_layout;
     enum AVSampleFormat sample_format;
     int                 sample_rate;

common/libavsmash_video.c
@@ -240,15 +240,22 @@ int libavsmash_get_video_frame
 #define MAX_ERROR_COUNT 3       /* arbitrary */
     codec_configuration_t *config  = &vdhp->config;
     AVFrame               *picture = vdhp->frame_buffer;
+    uint32_t               config_index;
     if( sample_number < vdhp->first_valid_frame_number || sample_count == 1 )
     {
+        /* Get the index of the decoder configuration. */
+        lsmash_sample_t sample;
+        uint32_t decoding_sample_number = get_decoding_sample_number( vdhp->order_converter, vdhp->first_valid_frame_number );
+        if( lsmash_get_sample_info_from_media_timeline( vdhp->root, vdhp->track_ID, decoding_sample_number, &sample ) < 0 )
+            goto video_fail;
+        config_index = sample.index;
         /* Copy the first valid video frame data. */
         av_frame_unref( picture );
         if( av_frame_ref( picture, vdhp->first_valid_frame ) < 0 )
             goto video_fail;
         /* Force seeking at the next access for valid video frame. */
         vdhp->last_sample_number = sample_count + 1;
-        return 0;
+        goto return_frame;
     }
     uint32_t start_number;  /* number of sample, for normal decoding, where decoding starts excluding decoding delay */
     uint32_t rap_number;    /* number of sample, for seeking, where decoding starts excluding decoding delay */
@@ -310,6 +317,14 @@ int libavsmash_get_video_frame
         start_number = seek_video( vdhp, picture, sample_number, rap_number, roll_recovery || seek_mode != SEEK_MODE_NORMAL );
     }
     vdhp->last_sample_number = sample_number;
+    config_index = config->index;
+return_frame:;
+    /* Don't exceed the maximum presentation size specified for each sequence. */
+    extended_summary_t *extended = &config->entries[ config_index - 1 ].extended;
+    if( config->ctx->width > extended->width )
+        config->ctx->width = extended->width;
+    if( config->ctx->height > extended->height )
+        config->ctx->height = extended->height;
     return 0;
 video_fail:
     /* fatal error of decoding */

commit fe48aba3d9539c0e28f16eeadf898dfd2ebce283

libavsmash: Set up the maximum presentation size even if no update of decoder configuration.
Fix a regression in b376363be5 .

common/libavsmash.c
@@ -616,6 +616,15 @@ void update_configuration( lsmash_root_t *root, uint32_t track_ID, codec_configu
         /* Don't update the decoder configuration if L-SMASH cannot recognize CODEC or extract its specific info correctly. */
         config->index = config->queue.index;
         libavsmash_flush_buffers( config );
+        /* Set up the maximum presentation width and height. */
+        libavsmash_summary_t *entry   = config->index <= config->count ? &config->entries[ config->index - 1 ] : NULL;
+        lsmash_summary_t     *summary = entry ? entry->summary : NULL;
+        if( summary && summary->summary_type == LSMASH_SUMMARY_TYPE_VIDEO )
+        {
+            lsmash_video_summary_t *video = (lsmash_video_summary_t *)summary;
+            entry->extended.width  = video->width;
+            entry->extended.height = video->height;
+        }
         return;
     }
     AVCodecContext *ctx   = config->ctx;