2e054007c0a29ba0d39af41f69d7d39432eb4d5f
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2013-07-30 06:38:00 +0900

    lwinput: More pixel format and color space supports for VPY reader.

--- AviUtl/vpy_input.c
+++ AviUtl/vpy_input.c
@@ -33,13 +33,22 @@
 #include "VSScript.h"
 #include "VSHelper.h"
 
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+
+#include "colorspace.h"
+#include "video_output.h"
+
 typedef struct
 {
     const VSAPI       *vsapi;
     VSScript          *vsscript;
     VSNodeRef         *node;
     const VSVideoInfo *vi;
-    int                buf_linesize;
+    /* Video stuff */
+    AVFrame                  *av_frame;
+    AVCodecContext           *ctx;
+    lw_video_output_handler_t voh;
 } vpy_handler_t;
 
 static void *open_file
@@ -55,7 +64,7 @@ static void *open_file
     char *ext = &file_name[file_name_length - 4];
     if( ext[0] != '.' || ext[1] != 'v' || ext[2] != 'p' || ext[3] != 'y' )
         return NULL;
-    /* Try to open the file as avisynth script. */
+    /* Try to open the file as VapourSynth script. */
     vpy_handler_t *hp = lw_malloc_zero( sizeof(vpy_handler_t) );
     if( !hp )
         return NULL;
@@ -71,6 +80,10 @@ static void *open_file
     if( !hp->node )
         goto fail;
     hp->vi = hp->vsapi->getVideoInfo( hp->node );
+    /* */
+    hp->ctx = avcodec_alloc_context3( NULL );
+    if( !hp->ctx )
+        goto fail;
     return hp;
 fail:
     if( hp->node )
@@ -88,7 +101,50 @@ static int get_video_track
 )
 {
     vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
-    return isConstantFormat( hp->vi ) && hp->vi->numFrames > 0 ? 0 : -1;
+    if( !isConstantFormat( hp->vi ) || hp->vi->numFrames <= 0 )
+        return -1;
+    hp->av_frame = av_frame_alloc();
+    return hp->av_frame ? 0 : -1;
+}
+
+static enum AVPixelFormat vs_to_av_input_pixel_format
+(
+    VSPresetFormat vs_input_pixel_format
+)
+{
+    static const struct
+    {
+        VSPresetFormat     vs_input_pixel_format;
+        enum AVPixelFormat av_input_pixel_format;
+    } format_table[] =
+        {
+            { pfYUV420P8,  AV_PIX_FMT_YUV420P     },
+            { pfYUV422P8,  AV_PIX_FMT_YUV422P     },
+            { pfYUV444P8,  AV_PIX_FMT_YUV444P     },
+            { pfYUV410P8,  AV_PIX_FMT_YUV410P     },
+            { pfYUV411P8,  AV_PIX_FMT_YUV411P     },
+            { pfYUV440P8,  AV_PIX_FMT_YUV440P     },
+            { pfYUV420P9,  AV_PIX_FMT_YUV420P9LE  },
+            { pfYUV422P9,  AV_PIX_FMT_YUV422P9LE  },
+            { pfYUV444P9,  AV_PIX_FMT_YUV444P9LE  },
+            { pfYUV420P10, AV_PIX_FMT_YUV420P10LE },
+            { pfYUV422P10, AV_PIX_FMT_YUV422P10LE },
+            { pfYUV444P10, AV_PIX_FMT_YUV444P10LE },
+            { pfYUV420P16, AV_PIX_FMT_YUV420P16LE },
+            { pfYUV422P16, AV_PIX_FMT_YUV422P16LE },
+            { pfYUV444P16, AV_PIX_FMT_YUV444P16LE },
+            { pfRGB24,     AV_PIX_FMT_GBRP        },
+            { pfRGB27,     AV_PIX_FMT_GBRP9LE     },
+            { pfRGB30,     AV_PIX_FMT_GBRP10LE    },
+            { pfRGB48,     AV_PIX_FMT_GBRP16LE    },
+            { pfGray8,     AV_PIX_FMT_GRAY8       },
+            { pfGray16,    AV_PIX_FMT_GRAY16LE    },
+            { pfNone,      AV_PIX_FMT_NONE        }
+        };
+    for( int i = 0; format_table[i].vs_input_pixel_format != pfNone; i++ )
+        if( vs_input_pixel_format == format_table[i].vs_input_pixel_format )
+            return format_table[i].av_input_pixel_format;
+    return AV_PIX_FMT_NONE;
 }
 
 static int prepare_video_decoding
@@ -101,26 +157,23 @@ static int prepare_video_decoding
     h->video_sample_count = hp->vi->numFrames;
     h->framerate_num      = hp->vi->fpsNum;
     h->framerate_den      = hp->vi->fpsDen;
-    /* BITMAPINFOHEADER */
-    const VSFormat *format = hp->vi->format;
-    h->video_format.biSize        = sizeof( BITMAPINFOHEADER );
-    h->video_format.biWidth       = hp->vi->width;
-    h->video_format.biHeight      = hp->vi->height;
-    switch( format->id )
-    {
-        case pfRGB24 :
-            h->video_format.biBitCount    = RGB24_SIZE << 3;
-            h->video_format.biCompression = OUTPUT_TAG_RGB;
-            break;
-        case pfYUV422P8 :
-            h->video_format.biBitCount    = YUY2_SIZE << 3;
-            h->video_format.biCompression = OUTPUT_TAG_YUY2;
-            break;
-        default :
-            return -1;
-    }
-    hp->buf_linesize = MAKE_AVIUTL_PITCH( hp->vi->width * h->video_format.biBitCount );
+    /* Set up the initial input format. */
+    hp->ctx->width      = hp->vi->width;
+    hp->ctx->height     = hp->vi->height;
+    hp->ctx->pix_fmt    = vs_to_av_input_pixel_format( hp->vi->format->id );
+    hp->ctx->colorspace = AVCOL_SPC_UNSPECIFIED;
+    /* Set up video rendering. */
+    if( !au_setup_video_rendering( &hp->voh, hp->ctx, opt, &h->video_format, hp->vi->width, hp->vi->height ) )
+        return -1;
     return 0;
+}
+
+static inline int vs_is_rgb_format
+(
+    int color_family
+)
+{
+    return color_family >= cmRGB && color_family < cmRGB + 1000000 ? 1 : 0;
 }
 
 static int read_video
@@ -131,64 +184,35 @@ static int read_video
 )
 {
     vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
-    const VSFrameRef *frame = hp->vsapi->getFrame( sample_number, hp->node, NULL, 0 );
-    if( !frame )
+    const VSFrameRef *vs_frame = hp->vsapi->getFrame( sample_number, hp->node, NULL, 0 );
+    if( !vs_frame )
         return 0;
-    const VSFormat *format = hp->vi->format;
-    if( h->video_format.biCompression == OUTPUT_TAG_RGB )
+    int is_rgb = vs_is_rgb_format( hp->vi->format->colorFamily );
+    for( int i = 0; i < hp->vi->format->numPlanes; i++ )
     {
-        int src_stride   = hp->vsapi->getStride( frame, 0 );
-        int src_offset   = 0;
-        int dst_offset   = hp->buf_linesize * (hp->vi->height - 1);
-        int dst_row_size = hp->vi->width * format->bytesPerSample * format->numPlanes;
-        const uint8_t *r = hp->vsapi->getReadPtr( frame, 0 );
-        const uint8_t *g = hp->vsapi->getReadPtr( frame, 1 );
-        const uint8_t *b = hp->vsapi->getReadPtr( frame, 2 );
-        for( int y = 0; y < hp->vi->height; y++ )
-        {
-            const uint8_t *src_data[3] = { b + src_offset, g + src_offset, r + src_offset };
-            uint8_t *dst_data = buf + dst_offset;
-            uint8_t *dst_end  = dst_data + dst_row_size;
-            while( dst_data < dst_end )
+        static const int component_reorder[2][3] =
             {
-                (*dst_data++) = *(src_data[0]++);
-                (*dst_data++) = *(src_data[1]++);
-                (*dst_data++) = *(src_data[2]++);
-            }
-            src_offset += src_stride;
-            dst_offset -= hp->buf_linesize;
-        }
+                { 0, 1, 2 },    /* YUV -> YUV */
+                { 2, 0, 1 }     /* RGB -> GBR */
+            };
+        int j = component_reorder[is_rgb][i];
+        hp->av_frame->data    [j] = (uint8_t *)hp->vsapi->getReadPtr( vs_frame, i );
+        hp->av_frame->linesize[j] = hp->vsapi->getStride( vs_frame, i );
     }
-    else
-    {
-        int src_y_stride  = hp->vsapi->getStride( frame, 0 );
-        int src_uv_stride = hp->vsapi->getStride( frame, 1 );
-        int src_y_offset  = 0;
-        int src_uv_offset = 0;
-        int dst_offset    = 0;
-        int dst_row_size  = hp->vi->width * format->bytesPerSample * 2;
-        const uint8_t *y = hp->vsapi->getReadPtr( frame, 0 );
-        const uint8_t *u = hp->vsapi->getReadPtr( frame, 1 );
-        const uint8_t *v = hp->vsapi->getReadPtr( frame, 2 );
-        for( int i = 0; i < hp->vi->height; i++ )
-        {
-            const uint8_t *src_data[3] = { y + src_y_offset, u + src_uv_offset, v + src_uv_offset };
-            uint8_t *dst_data = buf + dst_offset;
-            uint8_t *dst_end  = dst_data + dst_row_size;
-            while( dst_data < dst_end )
-            {
-                (*dst_data++) = *(src_data[0]++);
-                (*dst_data++) = *(src_data[1]++);
-                (*dst_data++) = *(src_data[0]++);
-                (*dst_data++) = *(src_data[2]++);
-            }
-            src_y_offset  += src_y_stride;
-            src_uv_offset += src_uv_stride;
-            dst_offset    += hp->buf_linesize;
-        }
-    }
-    hp->vsapi->freeFrame( frame );
-    return hp->buf_linesize * hp->vi->height;
+    hp->av_frame->format = hp->ctx->pix_fmt;
+    int frame_size = convert_colorspace( &hp->voh, hp->ctx, hp->av_frame, buf );
+    hp->vsapi->freeFrame( vs_frame );
+    return frame_size;
+}
+
+static void video_cleanup( lsmash_handler_t *h )
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    if( !hp )
+        return;
+    if( hp->av_frame )
+        av_frame_free( &hp->av_frame );
+    lw_cleanup_video_output_handler( &hp->voh );
 }
 
 static void close_file
@@ -199,6 +223,8 @@ static void close_file
     vpy_handler_t *hp = (vpy_handler_t *)private_stuff;
     if( !hp )
         return;
+    if( hp->ctx )
+        avcodec_close( hp->ctx );
     if( hp->node )
         hp->vsapi->freeNode( hp->node );
     if( hp->vsscript )
@@ -220,7 +246,7 @@ lsmash_reader_t vpy_reader =
     NULL,
     NULL,
     NULL,
-    NULL,
+    video_cleanup,
     NULL,
     close_file
 };
