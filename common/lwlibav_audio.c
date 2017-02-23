c53abb9548924b6500bec596c538e054ead20089
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2017-02-24 02:57:31 +0900

    lwlibav_audio: Fix flush of the last audio frame when requested length is smaller than its length.

--- common/lwlibav_audio.c
+++ common/lwlibav_audio.c
@@ -582,13 +582,14 @@ retry_seek:
         else if( frame_number > adhp->frame_count )
         {
             av_packet_unref( pkt );
-            if( adhp->exh.delay_count )
+            if( adhp->exh.delay_count || !(output_flags & AUDIO_OUTPUT_ENOUGH) )
             {
                 /* Null packet */
                 av_init_packet( pkt );
                 make_null_packet( pkt );
                 *alter_pkt = *pkt;
-                -- adhp->exh.delay_count;
+                if( adhp->exh.delay_count )
+                    adhp->exh.delay_count -= 1;
             }
             else
                 goto audio_out;
